97b9b0391f64a8b776de2c852477ea2d54280e53
nikolaystavrev <n.stavrev@abv.bg>
2015-03-15 12:08:33 +0200

    commit

--- public/javascripts/angularjs/directives/autocomplete.js
+++ public/javascripts/angularjs/directives/autocomplete.js
@@ -1,12 +1,11 @@
 define(['app'], function (app) {
     app.directive('autoComplete', function() {
        return function(scope, element, attrs){
-           scope.$watch('selected', function() {
                    element.autocomplete({
                        source : "/autocomplete",
                        html : true
                    });
-           });
+
        }
     });
 });
