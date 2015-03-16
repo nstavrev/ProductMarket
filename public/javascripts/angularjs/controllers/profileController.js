a19d9a50087a3683509bae933f402e628e9492fd
nikolaystavrev <n.stavrev@abv.bg>
2015-03-16 18:47:31 +0200

    no message

--- public/javascripts/angularjs/controllers/profileController.js
+++ public/javascripts/angularjs/controllers/profileController.js
@@ -1,9 +1,12 @@
 define(['app'], function (app) {
     app.register.controller('profileController', function($scope, ProfileService){
+        $scope.loading = true;
         ProfileService.getProfile().then(function(result){
+           $scope.loading = false;
            $scope.user = result.data;
         },
         function (error){
+            $scope.loading = false;
             $scope.user = undefined;
         });
     });
